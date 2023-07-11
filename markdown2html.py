f3f716bac148d0a90a76e7ffd93bc0d07a98db8f
wantonraven <josephngalu96@gmail.com>
2023-07-11 14:24:57 +0300

    solved pycode error

--- markdown2html.py
+++ markdown2html.py
@@ -8,31 +8,30 @@ import sys
 
 
 def convert_markdown_to_html(md, html):
-   """
+    """
     Converts a Markdown file to HTML.
 
     Args:
         md (str): Path to the input Markdown file.
         html (str): Path to the output HTML file.
     """
-    
+
     if not os.path.isfile(md):
         sys.stderr.write('Missing {}\n'.format(md))
         sys.exit(1)
 
     with open(md, 'r') as f:
         markdown_content = f.read()
-
-
     html_content = markdown.markdown(markdown_content)
 
     with open(html, 'w') as f:
         f.write(html_content)
 
+
 if __name__ == '__main__':
     if len(sys.argv) == 3:
-      convert_markdown_to_html(sys.argv[1], sys.argv[2])
+        convert_markdown_to_html(sys.argv[1], sys.argv[2])
     sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
     sys.exit(1)
-    
+
     sys.exit(0)
